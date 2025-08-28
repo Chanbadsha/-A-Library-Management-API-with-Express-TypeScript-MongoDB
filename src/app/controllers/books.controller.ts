5e7f09d028f84f6ef74e8dbad8c05518dc0edcd0
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2025-08-29 00:13:07 +0600

    Implement function to get all data ,filter ,sort,limit data

--- src/app/controllers/books.controller.ts
+++ src/app/controllers/books.controller.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express'
+import express, { json, Request, Response } from 'express'
 import { Books } from '../models/books.model'
 
 export const bookRoutes = express.Router()
@@ -13,6 +13,34 @@ bookRoutes.post("/", async (req: Request, res: Response) => {
             "success": true,
             "message": "Book created successfully",
             "data": book
+        })
+    } catch (error: any) {
+        res.status(401).json({
+            succeess: false,
+            message: error.message,
+            error
+        })
+    }
+})
+
+
+// A api for get all books
+
+bookRoutes.get("/", async (req: Request, res: Response) => {
+    try {
+        const { filter, genre, Genre, sortBy = "createdAt", sort, limit } = req.query
+        const appliedGenre = filter || genre || Genre
+
+        const filterData = appliedGenre ? { genre: appliedGenre } : {}
+        const limited = typeof limit === "string" ? parseInt(limit) : 10
+
+        const sortedData = sort === "asc" ? "asc" : "desc"
+        const books = await Books.find(filterData).sort({ [sortBy as string]: sortedData }).limit(limited)
+
+        res.status(200).json({
+            "success": true,
+            "message": "Books retrieved successfully",
+            "data": books
         })
     } catch (error: any) {
         res.status(401).json({
