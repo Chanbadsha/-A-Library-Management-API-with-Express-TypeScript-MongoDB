10c6c0b030cfe899000605d40fa559bfe26c83c9
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2025-09-02 12:14:10 +0600

    Implement function to reduse the borrowd copies

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -19,13 +19,6 @@ borrowRoutes.post('/', async (req: Request, res: Response) => {
         }
 
 
-        const bookDoc = await Books.findById(bookId);
-        if (!bookDoc) throw new Error("Book not found");
-
-        bookDoc.copies -= quantity;
-        if (bookDoc.copies === 0) bookDoc.available = false;
-        await bookDoc.save();
-
         const borrow = await Borrow.create({ book, quantity, dueDate })
 
         res.status(200).json({
--- src/app/models/borrow.model.ts
+++ src/app/models/borrow.model.ts
@@ -1,5 +1,6 @@
 import { model, Schema } from "mongoose";
 import { IBorrow } from "../interfaces/borrow.interface";
+import { Books } from "./books.model";
 
 const borrowSchema = new Schema<IBorrow>({
     book: {
@@ -31,5 +32,27 @@ const borrowSchema = new Schema<IBorrow>({
         versionKey: false,
         timestamps: true
     })
+
+
+
+// Middleware: reduce copies before saving
+borrowSchema.pre("save", async function (next) {
+    const borrow = this as any;
+
+    const book = await Books.findById(borrow.book);
+    if (!book) {
+        return next(new Error("Book not found"));
+    }
+
+    if (book.copies < borrow.quantity) {
+        return next(new Error("Not enough copies available"));
+    }
+
+    book.copies -= borrow.quantity;
+    if (book.copies === 0) book.available = false;
+    await book.save();
+
+    next();
+});
 
 export const Borrow = model<IBorrow>("Borrow", borrowSchema)
